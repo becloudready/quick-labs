bb8561b06f4fcd2a6ec4057b52a58cc3d69de4c7
Chandan Kumar <chandank@becloudready.com>
2019-06-26 21:37:49 -0400

    fixed typo

--- mysql/import_db.sh
+++ mysql/import_db.sh
@@ -3,4 +3,4 @@ DUMP_FILE=mysqlsampledatabase.sql
 HOST=mydb1.ctiembqzvsd8.us-east-1.rds.amazonaws.com
 USER=root
 
-mysql -h$HOST -u $USE  -p  < $DUMP_FILE 
+mysql -h$HOST -u$USER  -p  < $DUMP_FILE 
