7f9a25eb7b2d4b7fbca91f9ee62107cb00a31c54
Chandan Kumar <chandank@becloudready.com>
2020-09-03 21:05:12 -0400

    added sql

--- mysql/create-instance.py
+++ mysql/create-instance.py
@@ -1,12 +1,32 @@
+# import boto3
+
+# for i in range(21,25):
+
+#     rds = boto3.client('rds')
+#     try:
+#         response = rds.create_db_instance(
+        
+#         DBInstanceIdentifier='database-{}'.format(i),
+#         MasterUsername='admin',
+#         MasterUserPassword='admin123',
+#         DBInstanceClass='db.t2.micro',
+#         Engine='mysql',
+#         MultiAZ=False,
+#         PubliclyAccessible=True,
+#         DeletionProtection=False,
+#         AllocatedStorage=5)
+#         print (response)
+#     except Exception as error:
+#         print (error)
+
 import boto3
 
-# for i in range(16):
-i=1
+
 rds = boto3.client('rds')
 try:
     response = rds.create_db_instance(
     
-    DBInstanceIdentifier='database-{}'.format(i),
+    DBInstanceIdentifier='database-1',
     MasterUsername='admin',
     MasterUserPassword='admin123',
     DBInstanceClass='db.t2.micro',
